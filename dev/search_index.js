378585232bfdd9db94eea6283557ccf980e09ed6
Documenter.jl <documenter@juliadocs.github.io>
2022-12-03 21:58:12 +0000

    build based on faa0dcc

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = WhyNotEqual","category":"page"},{"location":"#WhyNotEqual","page":"Home","title":"WhyNotEqual","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for WhyNotEqual.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [WhyNotEqual]","category":"page"}]
+}
